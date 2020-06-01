f1d444efde184b5db9c5a37e577d8f1089960efb
trevol <iamvovan@mail.ru>
2020-06-01 12:20:15 +0300

    YoloDetector

---
+++ my_src/YoloDetector.py
@@ -0,0 +1,64 @@
+import torch
+from torch.autograd import Variable
+from torchvision.transforms import transforms
+from trvo_utils.imutils import imSize
+
+from models import Darknet
+from utils.datasets import pad_to_square, resize
+from utils.utils import non_max_suppression, rescale_boxes
+
+
+class YoloDetector:
+    toTensor = transforms.ToTensor()
+
+    def __init__(self, model_def, device, input_size, weights, defaultConfThreshold, defaultNmsThreshold):
+        self.device = torch.device(device)
+        self.model = Darknet(model_def, img_size=input_size).to(self.device)
+        self.model.load_state_dict(torch.load(weights))
+        self.model.eval()
+
+        self.defaultNmsThreshold = defaultNmsThreshold
+        self.defaultConfThreshold = defaultConfThreshold
+
+    def _preprocess(self, img):
+        imgTensor = self.toTensor(img)
+        imgTensor, _ = pad_to_square(imgTensor, 0)
+        imgTensor = resize(imgTensor, self.model.img_size)
+        return imgTensor
+
+    def _inputBatch(self, imgs):
+        batch = [self._preprocess(img) for img in imgs]
+        batch = torch.stack(batch)
+        if self.device.type == "cuda":
+            batch = batch.to("cuda")
+        return batch
+
+    def detectOnBatch(self, imgs, confThreshold=None, nmsThreshold=None):
+        assert len(imgs)
+        confThreshold = confThreshold or self.defaultConfThreshold or .5
+        nmsThreshold = nmsThreshold or self.defaultNmsThreshold or .4
+
+        batch = self._inputBatch(imgs)
+        input_var = Variable(batch)
+        with torch.no_grad():
+            detections = self.model(input_var)
+        detections = non_max_suppression(detections, confThreshold, nmsThreshold)
+
+        detections = [
+            rescale_boxes(imgDetections, self.model.img_size, imSize(img)) if imgDetections is not None else []
+            for imgDetections, img
+            in zip(detections, imgs)]
+        # x1, y1, x2, y2, conf, cls_conf, cls_pred
+        return detections
+
+    def detect(self, img, confThreshold=None, nmsThreshold=None):
+        batch = [img]
+        batchDetections = self.detectOnBatch(batch, confThreshold, nmsThreshold)
+        detections = batchDetections[0]
+        return detections
+
+    def __call__(self, imgOrBatch, confThreshold=None, nmsThreshold=None):
+        isBatch = isinstance(imgOrBatch, (list, tuple))
+        if isBatch:
+            return self.detectOnBatch(imgOrBatch, confThreshold, nmsThreshold)
+        return self.detect(imgOrBatch, confThreshold, nmsThreshold)
