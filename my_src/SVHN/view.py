381c4e8d4ca65d925a46f69ec7be9bcaaa6e314b
trevol <iamvovan@mail.ru>
2020-06-08 12:18:42 +0300

    SVHN dataset

--- my_src/SVHN/view.py
+++ my_src/SVHN/view.py
@@ -1,6 +1,8 @@
 import os
 import cv2
 from glob import glob
+
+from albumentations import BboxParams, Compose
 from trvo_utils import toInt
 from trvo_utils.annotation import PascalVocXmlParser
 from trvo_utils.imutils import imshowWait, rgb2bgr, imreadRGB, imSize
@@ -9,6 +11,7 @@ from trvo_utils.timer import timeit
 from my_src.SVHN import svhn_transforms
 from my_src.utils.MultiDirDataset import MultiDirDataset
 from tqdm import tqdm
+
 
 def main():
     test_dir = ['data/dataset/train']
@@ -28,6 +31,9 @@ def main():
 
 def main():
     inv = svhn_transforms.make(p=1)
+    bbox_params = BboxParams('pascal_voc', ['class_ids'], min_visibility=.5)
+    inv = Compose(inv, bbox_params)
+
     allLabels = list(map(str, range(10)))
     with timeit():
         images = []
@@ -43,8 +49,8 @@ def main():
             class_ids = [allLabels.index(l) for l in labels]
             annotations.append((boxes, labels))
             while True:
-                img_t = inv(image=img, bboxes=boxes)["image"]
-                imshowWait(rgb2bgr(img_t))
+                img_t = inv(image=img, bboxes=boxes, class_ids=class_ids)["image"]
+                if imshowWait(rgb2bgr(img_t)) == 27: return
 
 
 main()
